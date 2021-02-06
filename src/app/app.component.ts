d3e12e051b6bf6991c20a915e23f9747eb65075c
Rafee <rafeejenkins@gmail.com>
2021-02-06 00:36:15 +0000

    This is a test for the CI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'uo-hack';
+  
+  ngOnInit() {
+    console.log('test');
+  }
 }
