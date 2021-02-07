67c9f5a629abab53677f512065025d98169b1ac7
Rafee Jenkins <rafeejenkins@gmail.com>
2021-02-07 11:30:20 +0000

    UI Overhaul :wrench: (#20)
    * burger button tiome
    
    * added FAB menu
    
    * Finished tyranical UI attack :construction:

--- src/app/features/global/bottom-nav-bar/bottom-nav-bar.component.ts
+++ src/app/features/global/bottom-nav-bar/bottom-nav-bar.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { speedDialFabAnimations } from './speed-dial-fab.animations';
 
 @Component({
   selector: 'app-bottom-nav-bar',
   templateUrl: './bottom-nav-bar.component.html',
-  styleUrls: ['./bottom-nav-bar.component.scss']
+  styleUrls: ['./bottom-nav-bar.component.scss'],
+  animations: speedDialFabAnimations
 })
-export class BottomNavBarComponent implements OnInit {
+export class BottomNavBarComponent {
+
+  fabButtons = [
+    {
+      icon: 'add_location_alt',
+      route: ['/create']
+    },
+    {
+      icon: 'map',
+      route: ['/explorer']
+    },
+    {
+      icon: 'home',
+      route: ['/']
+    }
+  ];
+  
+  buttons: any = [];
+  fabTogglerState = 'inactive';
 
   constructor() { }
 
-  ngOnInit(): void {
+  showItems() {
+    this.fabTogglerState = 'active';
+    this.buttons = this.fabButtons;
+  }
+
+  hideItems() {
+    this.fabTogglerState = 'inactive';
+    this.buttons = [];
+  }
+
+  onToggleFab() {
+    this.buttons.length ? this.hideItems() : this.showItems();
   }
 
 }
